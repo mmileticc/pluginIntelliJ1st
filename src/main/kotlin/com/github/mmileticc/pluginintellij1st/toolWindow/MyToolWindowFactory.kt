5a2620b37526d058c4fad67b2f2f48b3bbce5d12
GitHub Action <action@github.com>
2025-12-11 23:59:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/mmileticc/pluginintellij1st/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mmileticc/pluginintellij1st/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mmileticc.pluginintellij1st.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mmileticc.pluginintellij1st.MyBundle
+import com.github.mmileticc.pluginintellij1st.services.MyProjectService
 import javax.swing.JButton
 
 
