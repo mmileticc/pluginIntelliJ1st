fcbb52c3b05afa4c51dbb2c9b2d5da285416cda0
milica <milicadjurdjevicc@gmail.com>
2025-12-13 16:35:07 +0100

    bojenje komentara

--- src/main/kotlin/com/github/mmileticc/pluginintellij1st/MyPluginLogic2.kt
+++ src/main/kotlin/com/github/mmileticc/pluginintellij1st/MyPluginLogic2.kt
@@ -1,11 +1,38 @@
 package com.github.mmileticc.pluginintellij1st
 
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.ui.JBColor
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtPsiFactory
+import java.awt.Font
+
+class MyCommentAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        if (element is PsiComment &&
+            element.text.startsWith("// MY_TAG")
+        ) {
+            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .textAttributes(
+                    TextAttributesKey.createTextAttributesKey(
+                    "MY_CUSTOM_COMMENT",
+                        TextAttributes(JBColor.magenta, null, null, null, Font.PLAIN)
+                ))
+                .range(element)
+                .create()
+        }
+    }
+}
+
 
 object MyPluginLogic2 {
     fun run(project: Project, file: PsiFile) {
@@ -15,7 +42,7 @@ object MyPluginLogic2 {
             val functions = file.children.filterIsInstance<KtNamedFunction>()
 
             for (funDecl in functions) {
-                val comment = factory.createComment("// Funkcija: ${funDecl.name}")
+                val comment = factory.createComment("// MY_TAG Funkcija: ${funDecl.name}")
                 file.addBefore(comment, funDecl)
             }
 
