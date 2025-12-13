4cac15f1243e90d5480621d85fad0c7b5acc0250
milica <milicadjurdjevicc@gmail.com>
2025-12-13 19:45:56 +0100

    dodate razlicite vrste brisanja

--- src/main/kotlin/com/github/mmileticc/pluginintellij1st/DeleteGeneratedCommentsAction.kt
+++ src/main/kotlin/com/github/mmileticc/pluginintellij1st/DeleteGeneratedCommentsAction.kt
@@ -38,3 +38,58 @@ class DeleteGeneratedCommentsAction : AnAction("Delete all generated comments") 
         }
     }
 }
+
+class DeleteNiceCommentsAction : AnAction("Delete all NICE comments") {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val file = e.getData(CommonDataKeys.PSI_FILE) ?: return
+
+        removeGeneratedComments(project, file)
+    }
+
+    private fun removeGeneratedComments(project: Project, file: PsiFile) {
+        WriteCommandAction.runWriteCommandAction(project) {
+            val comments = PsiTreeUtil.collectElementsOfType(file, PsiComment::class.java).toList()
+            for (c in comments) {
+                val text = c.text
+                if (text.contains("NICE:", ignoreCase=true)){
+                    c.delete()
+                }
+            }
+
+            val docManager = PsiDocumentManager.getInstance(project)
+            docManager.getDocument(file)?.let { doc ->
+                docManager.doPostponedOperationsAndUnblockDocument(doc)
+                docManager.commitDocument(doc)
+            }
+        }
+    }
+}
+
+class DeleteRoastCommentsAction : AnAction("Delete all ROAST and HELP comments") {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val file = e.getData(CommonDataKeys.PSI_FILE) ?: return
+
+        removeGeneratedComments(project, file)
+    }
+
+    private fun removeGeneratedComments(project: Project, file: PsiFile) {
+        WriteCommandAction.runWriteCommandAction(project) {
+            val comments = PsiTreeUtil.collectElementsOfType(file, PsiComment::class.java).toList()
+            for (c in comments) {
+                val text = c.text
+                if (text.contains("ROAST:", ignoreCase = true)
+                    || text.contains("HELP:", ignoreCase=true)){
+                    c.delete()
+                }
+            }
+
+            val docManager = PsiDocumentManager.getInstance(project)
+            docManager.getDocument(file)?.let { doc ->
+                docManager.doPostponedOperationsAndUnblockDocument(doc)
+                docManager.commitDocument(doc)
+            }
+        }
+    }
+}
